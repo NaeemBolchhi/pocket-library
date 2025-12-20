a8b1eff27e3e26e442190d7e50da298ac7f45173
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2025-12-20 15:53:13 +0600

    update url

--- dist/pl.meta.js
+++ dist/pl.meta.js
@@ -5,10 +5,10 @@
 // @description  Download articles, summaries, analyses, and notes from various English Literature websites as PDF.
 // @author       NaeemBolchhi
 // @license      GPL-3.0-or-later
-// @icon         data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.1' viewBox='0 0 300 300'%3E%3Cpath d='m284.11 232.24.02-.01-.1-.05.08.06zm-143.84 43.47-84.84-63.48-39.57 20L149.99 300l133.96-67.68-143.68 43.39z' fill='%23e64141'/%3E%3Cpath d='m284.14 232.29-.07-.08.05.09.02-.01zm-150.05-8.33-58.01-88.67-44.03 5.25L134.92 250.1l149.02-17.78-149.85-8.36z' fill='%23e64141'/%3E%3Cpath d='M146.01 173.14 121.83 70.01 78.64 59.89l59.19 138.14 146.31 34.3-138.13-59.19z' fill='%23e64141'/%3E%3Cpath d='m275.77 82.28 8.37 150.05-125.76-82.28L150.01 0l125.76 82.28z' fill='%23e64141'/%3E%3C/svg%3E
+// @icon         https://naeembolchhi.github.io/pocket-library/icon.svg
 // @match        http*://www.cliffsnotes.com/*
 // @run-at       document-idle
 // @grant        none
-// @downloadURL  main-url
-// @updateURL    meta-url
+// @downloadURL  https://naeembolchhi.github.io/pocket-library/dist/pl.user.js
+// @updateURL    https://naeembolchhi.github.io/pocket-library/dist/pl.meta.js
 // ==/UserScript==
--- src/1-0-global-const.part.js
+++ src/1-0-global-const.part.js
@@ -1,3 +1,3 @@
 // Globally available constants
-const fonturl = '',
-      pdfmake = '';
+const libfonts = 'https://naeembolchhi.github.io/pocket-library/lib/vfs_fonts.min.js',
+      libmain = 'https://naeembolchhi.github.io/pocket-library/lib/pdfmake.min.js';
