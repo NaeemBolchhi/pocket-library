c0860cc111401adfbe8fdf08b2ec66e1fff15faf
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2026-01-28 10:25:35 +0600

    add home and support urls

--- dist/pl.user.js
+++ dist/pl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pocket Library
 // @namespace    https://naeembolchhi.github.io/
-// @version      0.20260027182729
+// @version      0.20260028102507
 // @description  Download articles, summaries, analyses, and notes from various English Literature websites as PDF.
 // @author       NaeemBolchhi
 // @license      GPL-3.0-or-later
@@ -9,6 +9,8 @@
 // @match        http*://www.cliffsnotes.com/*
 // @run-at       document-body
 // @grant        none
+// @homepageURL  https://naeembolchhi.github.io/pocket-library/
+// @supportURL   https://github.com/NaeemBolchhi/pocket-library/issues
 // @downloadURL  https://naeembolchhi.github.io/pocket-library/dist/pl.user.js
 // @updateURL    https://naeembolchhi.github.io/pocket-library/dist/pl.meta.js
 // ==/UserScript==
